ccaec7f66e9a11c39032902ea6483f379f39a696
Devop-code <kamgadevop@gmail.com>
2025-02-03 20:15:48 +0100

    Ajout de la structure de base de l'application API avec les fichiers initiaux pour les utilisateurs, les migrations, les modèles, les vues et la configuration ASGI/WGI.

---
+++ backend/api/users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    USERNAME_FIELD = 'email'
+    email = models.EmailField(unique=True)
+    REQUIRED_FIELDS=[]
